4cfa26fe6b838dca0d579d4720ef85fd63258480
Catherine <catgsmith@hotmail.com>
2015-09-30 23:09:21 +0100

    add dataService for Parse

--- app/app.js
+++ app/app.js
@@ -1,13 +1,27 @@
 (function() {
  
-    var myApp = angular.module('myApp', []); // no dependencies
+    var myApp = angular.module('myApp', ['values']);
+
+    myApp.run(function (PARSE_CREDENTIALS) {	
+    	Parse.initialize(PARSE_CREDENTIALS.APP_ID, PARSE_CREDENTIALS.JAVASCRIPT_KEY);
+    });
+
     myApp.controller('myController', function ($scope, dataService){
 		$scope.appName = "Catherine";
 		
+		dataService.getDriver("kOOsFkwpKX").then(function (aDriver) { 
+
+			$scope.driverFirstName =  aDriver.get("firstName");
+			$scope.driverSurname =  aDriver.get("surname");
+			//console.log("Driver: " +  JSON.stringify(aDriver));
+		}, function(error) {
+			console.log("Parse error: " + error.message);		
+		});
+
 		$scope.vehicles = dataService.getVehicles();
+
 		
     });
-
  
 }());
 
--- app/services/dataService.js
+++ app/services/dataService.js
@@ -4,11 +4,38 @@
         .factory('dataService', dataService);
 
 
-    function dataService() {
+    function dataService($q) {
+    	// Declaring the type
+    	var Driver = Parse.Object.extend("Driver");
+
+   		Object.defineProperty(Driver, 'fullname', {
+    			get: function() {return (this.get("firstName") + " " + this.get("surname")); }
+    		});
+    		
+    
+		
         // Expose properties as API
         return {
+        	getDriver: getDriver,
             getVehicles: getVehicles
+
         };
+		function getDriver(jobId) {
+			var defer = $q.defer();
+ 
+			var driverQuery = new Parse.Query(Driver);
+			driverQuery.equalTo('objectId', jobId);
+			driverQuery.first({
+			 success: function(results) {
+			     defer.resolve(results);
+			 },
+			 error: function(error) {
+			     defer.reject(error);
+			 }
+			});
+
+			return defer.promise;
+		}
 
         function getVehicles() {
 
@@ -16,7 +43,7 @@
 			var vehicle = {};
 			vehicle.nextService = new Date(2015,10,1);
 			vehicle.reg = "11GH 1111";
-			vehicles.push(vehicle);  
+			vehicles.push(vehicle);
 
 			return vehicles;
         }
